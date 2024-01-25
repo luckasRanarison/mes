457335133cb640411e50bd1f3d9efa119ac6bdf2
Luckas <luckasranarison@gmail.com>
2024-01-25 10:00:18 +0300

    wip: skeleton

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,5 +1,9 @@
 mod register;
 
+use self::register::{
+    AddressRegiser, ControlRegister, DataRegister, MaskRegister, OamAddressRegister,
+    OamDataRegister, ScrollRegister, StatusRegister,
+};
 use crate::mappers::MapperRef;
 
 const VRAM_SIZE: usize = 16384;
@@ -10,6 +14,15 @@ pub struct Ppu {
     mapper: MapperRef,
     vram: [u8; VRAM_SIZE],
     sram: [u8; SRAM_SIZE],
+    latch: bool,
+    ppu_ctrl: ControlRegister,
+    ppu_mask: MaskRegister,
+    ppu_status: StatusRegister,
+    oam_addr: OamAddressRegister,
+    oam_data: OamDataRegister,
+    ppu_scroll: ScrollRegister,
+    ppu_addr: AddressRegiser,
+    ppu_data: DataRegister,
 }
 
 impl Ppu {
@@ -18,6 +31,15 @@ impl Ppu {
             mapper,
             vram: [0; VRAM_SIZE],
             sram: [0; SRAM_SIZE],
+            latch: false,
+            ppu_ctrl: ControlRegister::default(),
+            ppu_mask: MaskRegister::default(),
+            ppu_status: StatusRegister::default(),
+            oam_addr: OamAddressRegister::default(),
+            oam_data: OamDataRegister::default(),
+            ppu_scroll: ScrollRegister::default(),
+            ppu_addr: AddressRegiser::default(),
+            ppu_data: DataRegister::default(),
         }
     }
 }
--- src/ppu/register.rs
+++ src/ppu/register.rs
@@ -1,1 +1,29 @@
+#[derive(Debug, Default)]
+pub struct ControlRegister(u8);
 
+#[derive(Debug, Default)]
+pub struct MaskRegister(u8);
+
+#[derive(Debug, Default)]
+pub struct StatusRegister(u8);
+
+#[derive(Debug, Default)]
+pub struct OamAddressRegister {}
+
+#[derive(Debug, Default)]
+pub struct OamDataRegister {}
+
+#[derive(Debug, Default)]
+pub struct ScrollRegister {
+    low: u8,
+    high: u8,
+}
+
+#[derive(Debug, Default)]
+pub struct AddressRegiser {
+    low: u8,
+    high: u8,
+}
+
+#[derive(Debug, Default)]
+pub struct DataRegister {}
