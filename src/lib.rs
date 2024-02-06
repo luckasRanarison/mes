cd78bcd6e5b38f151e9a7f2404979cef6b88b849
Luckas <luckasranarison@gmail.com>
2024-02-06 08:26:45 +0300

    feat: core engine

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,54 @@
-pub mod bus;
-pub mod cartridge;
-pub mod controller;
-pub mod cpu;
-pub mod error;
-pub mod mappers;
-
+mod bus;
+mod cartridge;
+mod controller;
+mod cpu;
+mod error;
+mod mappers;
 mod ppu;
 mod utils;
 
 #[cfg(feature = "wasm")]
-pub mod wasm;
+mod wasm;
+
+use bus::MainBus;
+use cartridge::Cartridge;
+use cpu::Cpu;
+use error::Error;
+use mappers::get_mapper;
+
+#[derive(Debug)]
+pub struct Nes {
+    cpu: Cpu,
+}
+
+impl Nes {
+    pub fn new(bytes: &[u8]) -> Result<Nes, Error> {
+        let cartridge = Cartridge::try_from_bytes(bytes)?;
+        let mapper_id = cartridge.header.mapper;
+        let mapper = get_mapper(cartridge).ok_or(Error::UnsupportedMapper(mapper_id))?;
+        let bus = MainBus::new(mapper);
+        let cpu = Cpu::new(bus);
+
+        Ok(Self { cpu })
+    }
+
+    pub fn step_frame(&mut self) {
+        while !self.cpu.bus().is_vblank() {
+            self.cpu.step();
+        }
+    }
+
+    pub fn step_vblank(&mut self) {
+        while self.cpu.bus().is_vblank() {
+            self.cpu.step();
+        }
+    }
+
+    pub fn get_frame_buffer(&self) -> &[u8] {
+        self.cpu.bus().get_frame_buffer()
+    }
+
+    pub fn set_controller_state(&mut self, id: usize, state: u8) {
+        self.cpu.bus_mut().set_controller_state(id, state);
+    }
+}
--- src/wasm/mod.rs
+++ src/wasm/mod.rs
@@ -1,45 +1,37 @@
-use crate::{bus::MainBus, cartridge::Cartridge, cpu::Cpu, error::Error, mappers::get_mapper};
+use crate::Nes as NesEngine;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct Nes {
-    cpu: Cpu,
+    engine: NesEngine,
 }
 
 #[wasm_bindgen]
 impl Nes {
     #[wasm_bindgen(constructor)]
     pub fn new(bytes: &[u8]) -> Result<Nes, JsError> {
-        let cartridge = Cartridge::try_from_bytes(bytes)?;
-        let mapper_id = cartridge.header.mapper;
-        let mapper = get_mapper(cartridge).ok_or(Error::UnsupportedMapper(mapper_id))?;
-        let bus = MainBus::new(mapper);
-        let cpu = Cpu::new(bus);
-
-        Ok(Self { cpu })
+        Ok(Self {
+            engine: NesEngine::new(bytes)?,
+        })
     }
 
     #[wasm_bindgen(js_name = "stepFrame")]
     pub fn step_frame(&mut self) {
-        while !self.cpu.bus().is_vblank() {
-            self.cpu.step();
-        }
+        self.engine.step_frame();
     }
 
     #[wasm_bindgen(js_name = "stepVblank")]
     pub fn step_vblank(&mut self) {
-        while self.cpu.bus().is_vblank() {
-            self.cpu.step();
-        }
+        self.engine.step_vblank();
     }
 
     #[wasm_bindgen(js_name = "getFrameBufferPtr")]
     pub fn get_frame_buffer_ptr(&self) -> *const u8 {
-        self.cpu.bus().get_frame_buffer().as_ptr()
+        self.engine.get_frame_buffer().as_ptr()
     }
 
     #[wasm_bindgen(js_name = "setControllerState")]
     pub fn set_controller_state(&mut self, id: usize, state: u8) {
-        self.cpu.bus_mut().set_controller_state(id, state);
+        self.engine.set_controller_state(id, state);
     }
 }
