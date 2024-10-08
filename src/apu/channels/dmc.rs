80183a259c16b8e5c654f630e301bb20c6f4ed9f
luckasRanarison <luckasranarison@gmail.com>
2024-10-08 03:59:09 +0300

    wip: dmc redesigned

--- src/apu/channels/dmc.rs
+++ src/apu/channels/dmc.rs
@@ -9,67 +9,154 @@ use crate::{
 
 use super::common::{Channel, Timer};
 
-const RATES: [u16; 16] = [
-    428, 380, 340, 320, 286, 254, 226, 214, 190, 160, 142, 128, 106, 84, 72, 54,
-];
-
 #[derive(Debug)]
-pub struct Dmc {
-    enabled: bool,
-    irq_flag: bool,
-    loop_flag: bool,
-    output_level: u8,
-    sample_rate: u16,
+struct DmaReader {
+    mapper: MapperChip,
     sample_address: u16,
     sample_length: u16,
-    sample_buffer: u8,
+    sample_buffer: Option<u8>,
     remaining_bytes: u16,
     current_address: u16,
-    shift_register: u8,
-    shift_counter: u8,
-    silence_flag: bool,
     dma_cycles: Option<u8>,
-    timer: Timer,
-    mapper: MapperChip,
 }
 
-impl Dmc {
-    pub fn new(mapper: MapperChip) -> Self {
+impl DmaReader {
+    fn new(mapper: MapperChip) -> Self {
         Self {
             mapper,
-            enabled: false,
-            irq_flag: false,
-            loop_flag: false,
-            output_level: 0,
-            sample_rate: 0,
             sample_address: 0,
             sample_length: 0,
-            sample_buffer: 0,
+            sample_buffer: None,
             remaining_bytes: 0,
             current_address: 0,
-            shift_register: 0,
-            shift_counter: 0,
-            silence_flag: false,
-            dma_cycles: Some(0),
-            timer: Timer::default(),
+            dma_cycles: None,
         }
-    }
-
-    pub fn irq(&self) -> bool {
-        self.irq_flag
-    }
-
-    pub fn clear_irq(&mut self) {
-        self.irq_flag = false;
-    }
-
-    pub fn take_dma_cycles(&mut self) -> Option<u8> {
-        self.dma_cycles.take()
     }
 
     fn restart(&mut self) {
         self.current_address = self.sample_address;
         self.remaining_bytes = self.sample_length;
+    }
+
+    fn should_fetch(&self) -> bool {
+        self.remaining_bytes > 0 && self.sample_buffer.is_none()
+    }
+
+    fn fetch_byte(&mut self) -> u8 {
+        let byte = self.mapper.read(self.current_address);
+
+        self.sample_buffer = Some(byte);
+        self.remaining_bytes -= 1;
+        *self.dma_cycles.get_or_insert(0) += 4;
+
+        if self.current_address == 0xFFFF {
+            self.current_address = 0x8000;
+        } else {
+            self.current_address += 1;
+        }
+
+        byte
+    }
+}
+
+#[derive(Debug, Default)]
+struct OutputUnit {
+    buffer: Option<u8>,
+    shift_register: u8,
+    shift_counter: u8,
+    silence_flag: bool,
+    level: u8,
+}
+
+impl OutputUnit {
+    fn start_cycle(&mut self) {
+        self.shift_counter = 8;
+
+        if let Some(buffer) = self.buffer.take() {
+            self.silence_flag = false;
+            self.shift_register = buffer;
+        } else {
+            self.silence_flag = true;
+        }
+    }
+
+    fn shift(&mut self) {
+        match self.shift_register.contains(0) {
+            true if self.level < 126 => self.level += 2,
+            false if self.level > 2 => self.level -= 2,
+            _ => {}
+        }
+    }
+}
+
+impl Clock for OutputUnit {
+    fn tick(&mut self) {
+        if !self.silence_flag {
+            self.shift();
+        }
+
+        if self.shift_counter > 0 {
+            self.shift_counter -= 1;
+        }
+
+        if self.shift_counter == 0 {
+            self.start_cycle();
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Dmc {
+    enabled: bool,
+    irq_flag: bool,
+    irq_status: bool,
+    loop_flag: bool,
+    reader: DmaReader,
+    output: OutputUnit,
+    timer: Timer,
+}
+
+impl Dmc {
+    #[rustfmt::skip]
+    const RATES: [u16; 16] = [
+        428, 380, 340, 320, 286, 254, 226, 214,
+        190, 160, 142, 128, 106, 84, 72, 54,
+    ];
+
+    pub fn new(mapper: MapperChip) -> Self {
+        Self {
+            enabled: false,
+            irq_flag: false,
+            irq_status: false,
+            loop_flag: false,
+            reader: DmaReader::new(mapper),
+            output: OutputUnit::default(),
+            timer: Timer::default(),
+        }
+    }
+
+    pub fn irq(&self) -> bool {
+        self.irq_status
+    }
+
+    pub fn clear_irq(&mut self) {
+        self.irq_status = false;
+    }
+
+    pub fn take_dma_cycles(&mut self) -> Option<u8> {
+        self.reader.dma_cycles.take()
+    }
+
+    fn fetch_sample(&mut self) {
+        self.output.buffer = self.reader.fetch_byte().into();
+
+        if self.reader.remaining_bytes == 0 {
+            if self.loop_flag {
+                self.reader.restart();
+            } else {
+                self.irq_status = true;
+            }
+        }
     }
 }
 
@@ -79,21 +166,20 @@ impl Channel for Dmc {
             0 => {
                 self.irq_flag = value.contains(7);
                 self.loop_flag = value.contains(6);
-                self.sample_rate = RATES[value.get_range(0..4) as usize];
-                self.timer.period = self.sample_rate;
+                self.timer.period = Self::RATES[value.get_range(0..4) as usize] / 2;
             }
-            1 => self.output_level = value.get_range(0..7),
-            2 => self.sample_address = 0xC00 + (64 * value as u16),
-            _ => self.sample_length = (16 * value as u16) + 1,
+            1 => self.output.level = value.get_range(0..7),
+            2 => self.reader.sample_address = 0xC00 + (64 * value as u16),
+            _ => self.reader.sample_length = (16 * value as u16) + 1,
         }
     }
 
     fn raw_sample(&self) -> u8 {
-        self.output_level
+        self.output.level
     }
 
     fn is_active(&self) -> bool {
-        self.remaining_bytes > 0
+        self.reader.remaining_bytes > 0
     }
 
     fn is_mute(&self) -> bool {
@@ -104,9 +190,9 @@ impl Channel for Dmc {
         self.enabled = value;
 
         if !self.enabled {
-            self.remaining_bytes = 0;
+            self.reader.remaining_bytes = 0;
         } else {
-            self.restart();
+            self.reader.restart();
         }
     }
 }
@@ -120,7 +206,11 @@ impl Clock for Dmc {
         self.timer.tick();
 
         if self.timer.is_zero() {
-            // TODO
+            self.output.tick();
+        }
+
+        if self.reader.should_fetch() {
+            self.fetch_sample();
         }
     }
 }
